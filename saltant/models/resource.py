0683f6dba1a6f5a41e88dbc29ab7b8110966c604
Matt Wiens <mwiens91@gmail.com>
2018-08-31 10:17:08 -0700

    Add base model manager class

--- saltant/models/resource.py
+++ saltant/models/resource.py
@@ -1,11 +1,40 @@
 """Contains base classes for models and related classes."""
 
 
+class ModelManager(object):
+    """Base class for a model manager.
+
+    Attributes:
+        _client (:py:class:`saltant.client.Client`): An authenticated
+            saltant client.
+    """
+    def __init__(self, _client):
+        """Save the client so we can make API calls in the manager.
+
+        Args:
+            _client (:py:class:`saltant.client.Client`): An
+                authenticated saltant client.
+        """
+        self._client = _client
+
+    def list(self):
+        """List instances of models."""
+        raise NotImplementedError
+
+    def get(self):
+        """Get a specific instance of a model."""
+        raise NotImplementedError
+
+    def create(self):
+        """Create an instance of a model."""
+        raise NotImplementedError
+
+
 class Model(object):
     """Base class for representing a model.
 
     Attributes:
-        client (:py:class:`saltant.client.Client`): An authenticated
+        _client (:py:class:`saltant.client.Client`): An authenticated
             saltant client.
     """
     def __init__(self, _client):
