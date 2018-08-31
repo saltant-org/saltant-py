8fd8d47fe5c584389617109eabb2705611c4ad78
Matt Wiens <mwiens91@gmail.com>
2018-08-31 16:01:17 -0700

    Fix error with setup.py

--- saltant/__init__.py
+++ saltant/__init__.py
@@ -1,3 +1,8 @@
 """Import saltant client for convenient access."""
 
-from .client import Client, from_env
+# The try block is so that setup.py can peek into the directory without
+# having external packages installed.
+try:
+    from .client import Client, from_env
+except ImportError:
+    pass
--- saltant/version.py
+++ saltant/version.py
@@ -1,5 +1,5 @@
 """Contains name, version, and description."""
 
 NAME = 'saltant-py'
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 DESCRIPTION = "saltant SDK for Python"
